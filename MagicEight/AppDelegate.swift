b98da7ccacffb6364b47c60d562592b5b2bc29bc
Andrew Bowden <andrew.bowden@minigeek.org>
2016-08-17 21:27:08 +1000

    Initial Commit

---
+++ MagicEight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MagicEight
+//
+//  Created by Andrew Bowden on 17/08/2016.
+//  Copyright © 2016 minigeek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MagicEight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MagicEight
+//
+//  Created by Andrew Bowden on 17/08/2016.
+//  Copyright © 2016 minigeek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
